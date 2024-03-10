a759508f7718c1c1c5bbaf1c37b9f002f1f28ee5
Jack Wei <jack_weq@icloud.com>
2024-03-09 23:17:19 -0500

    working fine

--- src/prompter.py
+++ src/prompter.py
@@ -4,8 +4,6 @@ import pyperclip
 import threading
 import util
 
-# flag to terminate the program
-terminate = False
 prevSyllable = ''
 curSyllable = ''
 
@@ -14,11 +12,6 @@ auto_paste_mode = False
 long_word_mode = False
 
 # --------------------------------------------------------------
-
-def quit():
-    global terminate 
-    terminate = True
-
 def read_syllable():
     global curSyllable
     pyautogui.doubleClick()
@@ -52,24 +45,3 @@ def paste_word():
 def init():
     # load the whole word list
     util.load_word_list()
-
-# --------------------------------------------------------------
-
-# probbaly not gonna work
-if __name__ == '__main__':
-    keyboard.on_press_key("shift", lambda _:read_syllable())
-    keyboard.on_press_key("esc", lambda _:quit())
-
-    # load the whole word list
-    util.load_word_list()
-
-    # main loop
-    while not terminate:
-        if prevSyllable is not curSyllable and len(util.possible_words) != 0:
-            prompt_word()
-            if auto_paste_mode:
-                paste_word()
-            prevSyllable = curSyllable
-
-    print("Program quitted successfully!")
-    exit()
